2d4e0dcd04292c68fefb1f998cce79e826586531
devender.kumar6 <devender.kumar6@olaelectric>
2023-09-12 13:23:07 +0530

    second screen started

---
+++ app/src/main/java/com/sanjoo/analysedtasks/Screen2.kt
@@ -0,0 +1,11 @@
+package com.sanjoo.analysedtasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Screen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen2)
+    }
+}
